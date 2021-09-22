0f761a5bf8be47b715ed3911d9366a2b93181fd6
Ben Weibel <weibel.b@gmail.com>
2021-09-22 11:44:33 -0700

    Add specialty single post templates

---
+++ single-senske_service.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying location pages
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+get_header();
+
+wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-location' );
+
+?>
+	<main id="primary" class="site-main">
+		<?php
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/location/content', 'location' );
+		}
+		?>
+	</main><!-- #primary -->
+<?php
+get_footer();
