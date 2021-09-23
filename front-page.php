d5880e5db1548cecbda66eb9fc8dc16175622a93
Ben Weibel <weibel.b@gmail.com>
2021-09-22 18:59:02 -0700

    Add Hero component

--- front-page.php
+++ front-page.php
@@ -17,12 +17,32 @@ wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-front-page' );
 
 ?>
 	<main id="primary" class="site-main">
-		<?php
-		while ( have_posts() ) {
-			the_post();
-			get_template_part( 'flexible_hero.php' );
-		}
-		?>
+
+<?php
+	// Hero.
+	$hero = get_field('hero');
+
+	get_template_part( 'template-parts/acf/flexible_hero', 'homepage', ['hero' => $hero] );
+	// Service Plan.
+	$service_cards = get_field('service_card');
+	if ( $service_cards ){
+	?>
+		<section class="home-section">
+			<div class="service-plan-cards">
+			<?php
+			foreach( $service_cards as $card ) {
+				$service_plan_id = $card['service_plan'][0];
+				get_template_part('template-parts/services/program_card', 'homepage', ['plan_id' => $service_plan_id]);
+			}
+			?>
+			</div>
+		</section>
+	<?php
+	}
+
+?>
+
+
 	</main><!-- #primary -->
 <?php
 get_footer();
---
+++ template-parts/acf/flexible_hero.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template part for displaying a hero image area
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+// If we're passed an ID, use that
+if ( $args ) {
+	$hero = $args['hero'];
+} else {
+	$hero = $post;
+}
+
+?>
+
+<section class="hero fullwidth" style="background-image: url(<?php echo $hero['image']['url']; ?>)">
+	<h2 class="main-title"><?php echo $hero['title']; ?></h2>
+	<h4 class="secondary-title"><?php echo $hero['subtitle']; ?></h4>
+	<?php
+	$button = $hero['button'];
+
+	$args = array(
+		'text' => $button['title'],
+		'link'=> $button['link'],
+		'color' => 'yellow'
+	);
+	get_template_part( 'template-parts/components/button', '', $args );
+	?>
+</section><!-- .entry-content -->
