0e4d75df0367867f1024869dd71706bf412fff11
Ben Weibel <weibel.b@gmail.com>
2021-09-28 09:48:42 -0700

    Constrain Single Post image headers

--- inc/Sidebars/Component.php
+++ inc/Sidebars/Component.php
@@ -87,7 +87,7 @@ class Component implements Component_Interface, Templating_Component_Interface {
 		if ( $this->is_primary_sidebar_active() ) {
 			global $template;
 
-			if ( in_array( basename( $template ), array( 'single.php', 'archive.php', 'category.php', 'home.php' ) ) ) {
+			if ( in_array( basename( $template ), array( 'single.php', 'archive.php', 'category.php', 'home.php', 'single-senske_resource.php' ) ) ) {
 				$classes[] = 'has-sidebar';
 			}
 		}
