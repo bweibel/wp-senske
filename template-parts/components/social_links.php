d22004972f3a196c7b705b33f2f6449cfc83e2fa
Ben Weibel <weibel.b+gk@gmail.com>
2021-10-04 21:31:25 -0400

    Footer content and styling additions/updates

--- template-parts/components/social_links.php
+++ template-parts/components/social_links.php
@@ -9,10 +9,21 @@ namespace WP_Rig\WP_Rig;
 
 // $social_links = arr('facebook' => 'http://www.facebook.com',	'twitter' => 'http://www.twitter.com',	'youtube' => 'http://www.youtube.com');
 
+$theme = get_template_directory_uri();
+
 ?>
 
 <div class="social-links">
-	<a href="" class="facebook">Facebook</a>
-	<a href="" class="twitter">Twitter</a>
-	<a href="" class="youtube">Youtube</a>
+	<a href="https://www.facebook.com/SenskeServices" class="social-icon facebook" target="_blank" rel="nofollow">
+		<img src="<?php echo $theme . '/assets/images/senske-facebook.png'; ?>" alt="Facebook">
+	</a>
+	<a href="https://twitter.com/senskeservices" class="social-icon twitter" target="_blank" rel="nofollow">
+		<img src="<?php echo $theme . '/assets/images/senske-twitter.png'; ?>" alt="Twitter">
+	</a>
+	<a href="https://www.youtube.com/channel/UC3_O_6774OQDIauyKaDJFgA" class="social-icon youtube" target="_blank" rel="nofollow">
+		<img src="<?php echo $theme . '/assets/images/senske-youtube.png'; ?>" alt="Youtube">
+	</a>
+	<a href="https://www.instagram.com/senskeservices/" class="social-icon instagram" target="_blank" rel="nofollow">
+		<img src="<?php echo $theme . '/assets/images/senske-instagram.png'; ?>" alt="Instagram">
+	</a>
 </div>
--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -7,15 +7,19 @@
 
 namespace WP_Rig\WP_Rig;
 
+$theme = get_template_directory_uri();
+
 ?>
 
 <div class="site-info">
-	<span>Copyright 2021 Senske, Inc. All Rights Reserved</span>
-	<?php
-	/* translators: Theme name. */
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '<span class="sep"> | </span>' );
-	}
-	?>
-	<span class="sep"> | </span><span><a href="/site-map">Site Map</a></span>
+	<div class="footer-content">
+		<span>Copyright 2021 Senske, Inc. All Rights Reserved</span>
+		<span>
+			<img src="<?php echo $theme . '/assets/images/isa.png'; ?>" class="footer-logo" alt="ISA logo">
+			<img src="<?php echo $theme . '/assets/images/tcia-white-logo.png'; ?>" class="footer-logo" alt="TCIA logo">
+		</span>
+		<span>
+			<span><a href="/terms-conditions/">Terms and Conditions</a></span><span class="sep"> | </span><span><a href="/site-map">Site Map</a></span>
+		</span>
+	</div>
 </div><!-- .site-info -->
