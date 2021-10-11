26733eacaf3edc1eb28cf3f969a3fcc9d450fd64
Ben Weibel <=>
2021-10-11 13:23:16 -0700

    Add secondary image to expert cards

--- template-parts/frontpage/expert_card.php
+++ template-parts/frontpage/expert_card.php
@@ -11,12 +11,17 @@ namespace WP_Rig\WP_Rig;
 $expert = $args['expert'];
 $expert_fields = get_fields( $program );
 $expert_image = $expert['image'];
+$expert_small_image = $expert['secondary_image'];
 
 $card_class = 'card expert-card';
 ?>
 
 <article <?php post_class( $card_class  ); ?>>
-	<img src="<?php echo $expert['image']['url'];?>" alt="<?php echo $expert['image']['alt'];?>">
+	<div class="image-box">
+		<img class="card-image" src="<?php echo $expert['image']['url'];?>" alt="<?php echo $expert['image']['alt'];?>">
+	</div>
+	<img class="secondary-image" src="<?php echo $expert['secondary_image']['url'];?>" alt="<?php echo $expert['secondary_image']['alt'];?>">
+
 	<h3 class="expert-title"><?php echo $expert['title']; ?></h3>
 	<h4 class="expert-name mobile-hide"><?php echo $expert['name']; ?></h4>
 </article>
