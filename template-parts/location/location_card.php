ab64f94d13e8223aea457ef9ef1d8f42570383d8
Ben Weibel <weibel.b@gmail.com>
2023-01-24 14:13:58 -0800

    Add Yotpo toggle

--- template-parts/location/location_card.php
+++ template-parts/location/location_card.php
@@ -13,6 +13,7 @@ $location_ID = $post->ID;
 $location_name = $location->post_title;
 $phone = get_field('phone_number');
 $hours = get_field('hours');
+$yotpo_toggle = get_field('yotpo_toggle');
 $yotpo_id = get_field('yotpo_id');
 $address = get_field('address');
 
@@ -27,7 +28,9 @@ $address = get_field('address');
 		<?php echo $hours; ?>
 		<?php echo $address; ?>
 	</div>
+	<?php if ( $yotpo_toggle ): ?>
     <div class="yotpo bottomLine" data-appkey="F0TPJxIYZcYzPN4sr9IFjlzNs8Inxb9VYjYRYT74" data-product-id="<?php echo $yotpo_id; ?>"></div>
+	<? endif; ?>
 	<a href="/request-estimate/" class="button">Request your free estimate</a>
 
 </aside>
